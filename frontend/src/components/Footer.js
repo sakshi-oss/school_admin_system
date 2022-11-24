7d4cadb55321a18387bb7f3e6e34f0baf1df91e2
Pooja <poojakhandelwal1004@gmail.com>
2022-11-24 09:32:44 +0530

    School Admin System

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div id='footer'>
+     <h2>Team Members-</h2>
+      <h4>Sakshi Khandelwal (19ESKCS812)</h4>
+      <h4>Ritika Agarwal (19ESKCS804)</h4>
+      <h4>Rashika Joshi (19ESKCS196) </h4>
+      <h4>Rajat Vyas (19ESKCS195)  </h4>
+    </div>
+  )
+}
+
+export default Footer
